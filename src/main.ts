79b8e62abc51b6e2959e1eb0ff09af6e4fb0f039
Fikri Mutaqin <rpl.11.fikrimutaqin@gmail.com>
2026-05-06 22:38:03 +0700

    chore(main): edit main ts

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,50 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+let app: (arg0: any, arg1: any) => any;
 
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+async function createApp() {
+  const nestApp = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  nestApp.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
   });
 
-  app.enableCors();
+  nestApp.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('NexLearn LMS API')
-    .setDescription('The Learning Management System API documentation')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(nestApp, config);
 
-  SwaggerModule.setup('docs', app, document, {
-    customSiteTitle: 'NexLearn LMS API Docs',
-  });
+  SwaggerModule.setup('docs', nestApp, document);
 
-  app.useGlobalFilters(new AllExceptionsFilter());
-  app.useGlobalInterceptors(new TransformInterceptor());
+  nestApp.useGlobalFilters(new AllExceptionsFilter());
+  nestApp.useGlobalInterceptors(new TransformInterceptor());
 
-  app.useGlobalPipes(new ValidationPipe({
+  nestApp.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
   }));
 
-  // 🔥 WAJIB untuk Vercel
-  await app.init();
+  await nestApp.init();
 
-  return app.getHttpAdapter().getInstance();
+  return nestApp.getHttpAdapter().getInstance();
 }
 
-export default bootstrap;
+export default async function handler(req: any, res: any) {
+  if (!app) {
+    app = await createApp();
+  }
+  return app(req, res);
+}
